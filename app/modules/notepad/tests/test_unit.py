8adad93bf77c3261970a0f7f134055474566160a
Miguel Hernández Sánchez <mighersan1@alum.us.es>
2024-10-17 16:34:06 +0200

    feat(module): Notepad module

---
+++ app/modules/notepad/tests/test_unit.py
@@ -0,0 +1,37 @@
+import pytest
+
+from app import db
+from app.modules.conftest import login, logout
+from app.modules.auth.models import User
+from app.modules.profile.models import UserProfile
+
+
+@pytest.fixture(scope='module')
+def test_client(test_client):
+    """
+    Extends the test_client fixture to add additional specific data for module testing.
+    """
+    with test_client.application.app_context():
+        user_test = User(email='user@example.com', password='test1234')
+        db.session.add(user_test)
+        db.session.commit()
+
+        profile = UserProfile(user_id=user_test.id, name="Name", surname="Surname")
+        db.session.add(profile)
+        db.session.commit()
+
+    yield test_client
+
+
+def test_list_empty_notepad_get(test_client):
+    """
+    Tests access to the empty notepad list via GET request.
+    """
+    login_response = login(test_client, "user@example.com", "test1234")
+    assert login_response.status_code == 200, "Login was unsuccessful."
+
+    response = test_client.get("/notepad")
+    assert response.status_code == 200, "The notepad page could not be accessed."
+    assert b"You have no notepads." in response.data, "The expected content is not present on the page"
+
+    logout(test_client)
