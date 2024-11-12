c0f979c3a8d1b9a38a1c4d3de555758553c6bc26
Miguel Hernández Sánchez <mighersan1@alum.us.es>
2024-11-12 23:17:23 +0100

    fix: Selenium without interface
    Selenium test without interface in hubfile module.

--- app/modules/hubfile/tests/test_edit.py
+++ app/modules/hubfile/tests/test_edit.py
@@ -4,11 +4,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 
 
 class TestEdit():
     def setup_method(self, method):
-        self.driver = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        self.driver = webdriver.Chrome(chrome_options)
         self.vars = {}
 
     def teardown_method(self, method):
