3ce4144e2f65fb320e4264a627924db3dd1c7888
Daniel Benito Hidalgo <d4niel.bh@gmail.com>
2024-12-03 20:36:12 +0100

    feat: Add route to upload to zenodo
    Updated the routes of dataset to upload the dataset to zenodo once the publish button is pressed, before that, the dataset is stored only in the dtabase

--- app/modules/dataset/routes.py
+++ app/modules/dataset/routes.py
@@ -64,39 +64,8 @@ def create_dataset():
         except Exception as exc:
             logger.exception(f"Exception while create dataset data in local {exc}")
             return jsonify({"Exception while create dataset data in local: ": str(exc)}), 400
-
-        # send dataset as deposition to Zenodo
-        data = {}
-        try:
-            zenodo_response_json = zenodo_service.create_new_deposition(dataset)
-            response_data = json.dumps(zenodo_response_json)
-            data = json.loads(response_data)
-        except Exception as exc:
-            data = {}
-            zenodo_response_json = {}
-            logger.exception(f"Exception while create dataset data in Zenodo {exc}")
-
-        if data.get("conceptrecid"):
-            deposition_id = data.get("id")
-
-            # update dataset with deposition id in Zenodo
-            dataset_service.update_dsmetadata(dataset.ds_meta_data_id, deposition_id=deposition_id)
-
-            try:
-                # iterate for each feature model (one feature model = one request to Zenodo)
-                for feature_model in dataset.feature_models:
-                    zenodo_service.upload_file(dataset, deposition_id, feature_model)
-
-                # publish deposition
-                zenodo_service.publish_deposition(deposition_id)
-
-                # update DOI
-                deposition_doi = zenodo_service.get_doi(deposition_id)
-                dataset_service.update_dsmetadata(dataset.ds_meta_data_id, dataset_doi=deposition_doi)
-            except Exception as e:
-                msg = f"it has not been possible upload feature models in Zenodo and update the DOI: {e}"
-                return jsonify({"message": msg}), 200
-
+        
+        
         # Delete temp folder
         file_path = current_user.temp_folder()
         if os.path.exists(file_path) and os.path.isdir(file_path):
@@ -326,9 +295,42 @@ def publish_all_datasets():
 @dataset_bp.route("/dataset/<int:dataset_id>/publish", methods=["GET"])
 @login_required
 def publish_dataset(dataset_id):
-    dataset_service.publish_specific_dataset(current_user_id=current_user.id, dataset_id=dataset_id)
-    return render_template(
-        "dataset/list_datasets.html",
-        datasets=dataset_service.get_synchronized(current_user.id),
-        local_datasets=dataset_service.get_unsynchronized(current_user.id),
-    )
+    # Obtener el dataset
+    dataset = dataset_service.get_or_404(dataset_id)
+    dataset_service.publish_specific_dataset(current_user.id, dataset_id)
+    try:
+        # Send dataset to zenodo
+        zenodo_response_json = zenodo_service.create_new_deposition(dataset)
+        response_data = json.dumps(zenodo_response_json)
+        data = json.loads(response_data)
+    except Exception as exc:
+        logger.exception(f"Exception while creating dataset data in Zenodo: {exc}")
+        return jsonify({"message": "Error while creating dataset in Zenodo."}), 500
+
+    if data.get("conceptrecid"):
+        deposition_id = data.get("id")
+
+        #Update dataset with deposition ID from zeznodo
+        try:
+            dataset_service.update_dsmetadata(dataset.ds_meta_data_id, deposition_id=deposition_id)
+
+            for feature_model in dataset.feature_models:
+                try:
+                    zenodo_service.upload_file(dataset, deposition_id, feature_model)
+                except Exception as e:
+                    logger.exception(f"Error uploading feature model to Zenodo: {e}")
+                    continue 
+
+            zenodo_service.publish_deposition(deposition_id)
+
+            deposition_doi = zenodo_service.get_doi(deposition_id)
+            dataset_service.update_dsmetadata(dataset.ds_meta_data_id, dataset_doi=deposition_doi)
+
+        except Exception as e:
+            logger.exception(f"Error while processing Zenodo actions: {e}")
+            return jsonify({"message": f"Error during Zenodo publication process: {e}"}), 500
+
+        msg = "Dataset successfully published to Zenodo!"
+        return jsonify({"message": msg}), 200
+
+    return jsonify({"message": "Failed to create deposition on Zenodo."}), 400
