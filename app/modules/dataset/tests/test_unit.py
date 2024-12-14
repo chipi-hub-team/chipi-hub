3affa70878606cd1fb71d663a3957f8c2f04c9a4
Sergio Pons Lopez <serponlop@alum.us.es>
2024-12-14 12:34:51 +0100

    test: Unit test publish dataset success added
    A unit test has been added for publishing a dataset in the stagging area.

--- app/modules/dataset/tests/test_unit.py
+++ app/modules/dataset/tests/test_unit.py
@@ -1,4 +1,8 @@
 import pytest
+from unittest.mock import patch, MagicMock
+from app.modules.conftest import login, logout
+from app.modules.auth.models import User
+from app import db
 
 
 @pytest.fixture(scope="module")
@@ -9,7 +13,10 @@ def test_client(test_client):
     with test_client.application.app_context():
         # Add HERE new elements to the database that you want to exist in the test context.
         # DO NOT FORGET to use db.session.add(<element>) and db.session.commit() to save the data.
-        pass
+        # Create a test user
+        user_test = User(email="testuser@example.com", password="1234")
+        db.session.add(user_test)
+        db.session.commit()
 
     yield test_client
 
@@ -22,3 +29,42 @@ def test_download_all_dataset(test_client):
     assert response.status_code == 200, "Download all datasets was unsuccessful"
 
     test_client.get("/logout", follow_redirects=True)
+
+
+def test_publish_dataset_success(test_client):
+    """
+    Positive case: Publish an unpublished dataset.
+    """
+    dataset_id = 500
+    login_response = login(test_client, "testuser@example.com", "1234")
+    assert login_response.status_code == 200, "Login was failed."
+
+    with patch("app.modules.dataset.services.DataSetService.get_or_404") as mock_get_dataset, \
+         patch("app.modules.zenodo.services.ZenodoService.create_new_deposition") as mock_zenodo, \
+         patch("app.modules.zenodo.services.ZenodoService.upload_file") as mock_upload_file:
+
+        mock_get_dataset.return_value = MagicMock(
+            id=dataset_id,
+            ds_meta_data_id=1000,
+            ds_meta_data=MagicMock(
+                ds_status="unpublished",
+                title="Dataset 1",
+                description="Description of dataset 1",
+                publication_type="DATA_MANAGEMENT_PLAN",
+                dataset_doi=None,
+            ),
+            feature_models=[
+                MagicMock(id=101, name="FeatureModel1"),
+                MagicMock(id=102, name="FeatureModel2"),
+            ],
+        )
+
+        mock_upload_file.return_value = None
+        mock_zenodo.return_value = {"conceptrecid": "12345", "id": "67890"}
+
+        response = test_client.post(f"/dataset/{dataset_id}/publish", follow_redirects=True)
+        print("Response", response.get_json())
+        assert response.status_code == 200, "Publishing the unpublished dataset failed."
+        mock_get_dataset.assert_called_once_with(dataset_id)
+
+    logout(test_client)
