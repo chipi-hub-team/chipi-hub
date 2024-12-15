9179b85e4a42681a8deef380c8b42001ada376bb
Daniel Benito Hidalgo <d4niel.bh@gmail.com>
2024-12-15 12:46:09 +0100

    fix: Locust test updated
    Updated Locust test to fix Codacy Static Code Analysis

--- app/modules/dataset/tests/locustfile.py
+++ app/modules/dataset/tests/locustfile.py
@@ -1,7 +1,7 @@
 from locust import HttpUser, TaskSet, task
 from core.locust.common import get_csrf_token
 from core.environment.host import get_host_for_locust_testing
-import random
+import secrets
 
 
 class DatasetBehavior(TaskSet):
@@ -24,8 +24,10 @@ class DatasetUser(HttpUser):
 class DatasetPublishBehavior(TaskSet):
     @task
     def publish_dataset(self):
-        dataset_id = random.randint(1, 100)
+        # Genera un número aleatorio seguro para la publicación del dataset
+        dataset_id = secrets.randbelow(100) + 1  # Genera un número entre 1 y 100
         response = self.client.post(f"/dataset/{dataset_id}/publish")
+
         if response.status_code == 200:
             print(f"Dataset {dataset_id} publicado con éxito")
         elif response.status_code == 400:
